d3c1c72a06236d1f4dab001475941256a5ee1e73
Guillaume Bertholon <guillaume.bertholon@ens.fr>
2021-11-17 14:55:50 +0100

    Stop ignoring blanks at the begining of files

--- syndiff/src/syn_tree.rs
+++ syndiff/src/syn_tree.rs
@@ -9,14 +9,22 @@ impl<'t> SynNode<'t> {
     }
 }
 
-fn build_syn_tree<'t>(cursor: &mut tree_sitter::TreeCursor, source: &'t [u8]) -> SynNode<'t> {
+fn build_syn_tree<'t>(
+    cursor: &mut tree_sitter::TreeCursor,
+    source: &'t [u8],
+    root: bool,
+) -> SynNode<'t> {
     let node = cursor.node();
     if !node.is_named() {
         return SynNode(Tree::Leaf(Token::new(&source[node.byte_range()])));
     }
     let kind = node.kind_id();
     let mut children = Vec::new();
-    let mut cur_byte = node.start_byte();
+
+    // We cannot trust tree-sitter root node span, that is shorter than full file if the file
+    // starts with an ignored leaf (like space or new line).
+    let mut cur_byte = if root { 0 } else { node.start_byte() };
+
     if cursor.goto_first_child() {
         loop {
             let field = cursor.field_id();
@@ -28,7 +36,7 @@ fn build_syn_tree<'t>(cursor: &mut tree_sitter::TreeCursor, source: &'t [u8]) ->
                     node: SynNode(Tree::Leaf(Token::new(&source[cur_byte..start_byte]))),
                 });
             }
-            let subtree = build_syn_tree(cursor, source);
+            let subtree = build_syn_tree(cursor, source, false);
             children.push(Subtree {
                 field,
                 node: subtree,
@@ -40,10 +48,12 @@ fn build_syn_tree<'t>(cursor: &mut tree_sitter::TreeCursor, source: &'t [u8]) ->
         }
         assert!(cursor.goto_parent());
     }
-    if cur_byte < node.end_byte() {
+
+    let end_byte = if root { source.len() } else { node.end_byte() };
+    if cur_byte < end_byte {
         children.push(Subtree {
             field: None,
-            node: SynNode(Tree::Leaf(Token::new(&source[cur_byte..node.end_byte()]))),
+            node: SynNode(Tree::Leaf(Token::new(&source[cur_byte..end_byte]))),
         })
     }
     SynNode(Tree::Node(kind, children))
@@ -52,6 +62,6 @@ fn build_syn_tree<'t>(cursor: &mut tree_sitter::TreeCursor, source: &'t [u8]) ->
 pub fn parse_source<'t>(source: &'t [u8], parser: &mut Parser) -> Option<SynNode<'t>> {
     parser.reset();
     let tree = parser.parse(source, None)?;
-    let syn_tree = build_syn_tree(&mut tree.walk(), source);
+    let syn_tree = build_syn_tree(&mut tree.walk(), source, true);
     Some(syn_tree)
 }
